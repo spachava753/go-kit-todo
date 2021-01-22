ee62249c614530431726d6e03ff994b340bee74d
Shashank Pachava <spachava753@gmail.com>
2021-01-21 20:47:35 -0500

    Add user service logging middleware

---
+++ user/logging.go
@@ -0,0 +1,54 @@
+package user
+
+import (
+	"context"
+	"github.com/go-kit/kit/log"
+	"time"
+)
+
+type basicLoggingService struct {
+	logger log.Logger
+	UserService
+}
+
+func (s *basicLoggingService) CreateUser(ctx context.Context, name string) (u User, err error) {
+	defer func(begin time.Time) {
+		s.logger.Log("operation", "create", "name", name, "took", time.Since(begin), "err", err)
+	}(time.Now())
+	return s.UserService.CreateUser(ctx, name)
+}
+
+func (s *basicLoggingService) DeleteUser(ctx context.Context, userId string) (u User, err error) {
+	defer func(begin time.Time) {
+		s.logger.Log("operation", "delete", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
+	}(time.Now())
+	return s.UserService.DeleteUser(ctx, userId)
+}
+
+func (s *basicLoggingService) UpdateUser(ctx context.Context, user User) (u User, err error) {
+	defer func(begin time.Time) {
+		s.logger.Log("operation", "update", "updatedName", user.Name, "took", time.Since(begin), "err", err)
+	}(time.Now())
+	return s.UserService.UpdateUser(ctx, user)
+}
+
+func (s *basicLoggingService) GetUserById(ctx context.Context, userId string) (u User, err error) {
+	defer func(begin time.Time) {
+		s.logger.Log("operation", "get", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
+	}(time.Now())
+	return s.UserService.GetUserById(ctx, userId)
+}
+
+func (s *basicLoggingService) ListUsers(ctx context.Context) (us []User, err error) {
+	defer func(begin time.Time) {
+		s.logger.Log("operation", "list", "len", len(us), "took", time.Since(begin), "err", err)
+	}(time.Now())
+	return s.UserService.ListUsers(ctx)
+}
+
+func NewBasicLoggingService(logger log.Logger, s UserService) UserService {
+	return &basicLoggingService{
+		logger:      logger,
+		UserService: s,
+	}
+}
