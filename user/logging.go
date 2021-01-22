8989dd6c11269d9d81f54a27f82dd4e8b601c3a7
Shashank Pachava <spachava753@gmail.com>
2021-01-22 01:32:23 -0500

    Fix user service logging

--- user/logging.go
+++ user/logging.go
@@ -15,40 +15,40 @@ func (s *basicLoggingService) CreateUser(ctx context.Context, name string) (u Us
 	defer func(begin time.Time) {
 		s.logger.Log("operation", "create", "name", name, "took", time.Since(begin), "err", err)
 	}(time.Now())
-	return s.UserService.CreateUser(ctx, name)
+	return s.Service.CreateUser(ctx, name)
 }
 
 func (s *basicLoggingService) DeleteUser(ctx context.Context, userId string) (u User, err error) {
 	defer func(begin time.Time) {
 		s.logger.Log("operation", "delete", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
 	}(time.Now())
-	return s.UserService.DeleteUser(ctx, userId)
+	return s.Service.DeleteUser(ctx, userId)
 }
 
 func (s *basicLoggingService) UpdateUser(ctx context.Context, user User) (u User, err error) {
 	defer func(begin time.Time) {
 		s.logger.Log("operation", "update", "updatedName", user.Name, "took", time.Since(begin), "err", err)
 	}(time.Now())
-	return s.UserService.UpdateUser(ctx, user)
+	return s.Service.UpdateUser(ctx, user)
 }
 
 func (s *basicLoggingService) GetUserById(ctx context.Context, userId string) (u User, err error) {
 	defer func(begin time.Time) {
 		s.logger.Log("operation", "get", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
 	}(time.Now())
-	return s.UserService.GetUserById(ctx, userId)
+	return s.Service.GetUserById(ctx, userId)
 }
 
 func (s *basicLoggingService) ListUsers(ctx context.Context) (us []User, err error) {
 	defer func(begin time.Time) {
 		s.logger.Log("operation", "list", "len", len(us), "took", time.Since(begin), "err", err)
 	}(time.Now())
-	return s.UserService.ListUsers(ctx)
+	return s.Service.ListUsers(ctx)
 }
 
 func NewBasicLoggingService(logger log.Logger, s Service) Service {
 	return &basicLoggingService{
-		logger:      logger,
-		UserService: s,
+		logger:  logger,
+		Service: s,
 	}
 }
