fefe0e337e3ebde21d44be9c7447374af6b0874c
Shashank Pachava <spachava753@gmail.com>
2021-01-22 01:32:42 -0500

    Set up basic server

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"github.com/go-kit/kit/log"
+	"github.com/oklog/ulid/v2"
+	"github.com/spachava/go-kit-todo/user"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	flag.Parse()
+
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "timestamp", log.DefaultTimestampUTC)
+
+	t := time.Unix(1000000, 0)
+	userEntropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	var userService user.Service
+	userService = user.NewMemUserService(userEntropy, t)
+	userService = user.NewBasicLoggingService(log.With(logger, "component", "user"), userService)
+
+	//todoEntropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	//todoService := todo.NewMemTodoService(todoEntropy, t)
+	//todoService := todo.NewBasicLoggingService(log.With(logger, "component", "todo"), todoService)
+
+	r := user.MakeHandler(userService, logger)
+	http.ListenAndServe(":8080", r)
 }
