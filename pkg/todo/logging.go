515ef29ed8ef586ce9b03d8ad56d3d3517f27ccc
Shashank Pachava <spachava753@gmail.com>
2021-01-25 22:41:35 -0500

    Code cleanup

--- pkg/todo/logging.go
+++ pkg/todo/logging.go
@@ -13,35 +13,35 @@ type basicLoggingService struct {
 
 func (s *basicLoggingService) CreateTodo(ctx context.Context, text string, userId string) (t Todo, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "create", "text", text, "userId", userId, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "create", "text", text, "userId", userId, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.CreateTodo(ctx, text, userId)
 }
 
 func (s *basicLoggingService) DeleteTodo(ctx context.Context, todoId string) (t Todo, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "delete", "todoId", todoId, "text", t.Text, "done", t.Done, "userId", t.UserId, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "delete", "todoId", todoId, "text", t.Text, "done", t.Done, "userId", t.UserId, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.DeleteTodo(ctx, todoId)
 }
 
 func (s *basicLoggingService) UpdateTodo(ctx context.Context, todo Todo) (t Todo, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "update", "updatedText", t.Text, "updatedDone", t.Done, "updatedUserId", t.UserId, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "update", "updatedText", t.Text, "updatedDone", t.Done, "updatedUserId", t.UserId, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.UpdateTodo(ctx, todo)
 }
 
 func (s *basicLoggingService) GetTodoById(ctx context.Context, todoId string) (t Todo, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "get", "todoId", todoId, "text", t.Text, "done", t.Done, "userId", t.UserId, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "get", "todoId", todoId, "text", t.Text, "done", t.Done, "userId", t.UserId, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.GetTodoById(ctx, todoId)
 }
 
 func (s *basicLoggingService) ListTodosByUserId(ctx context.Context, userId string) (ts []Todo, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "list", "len", len(ts), "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "list", "len", len(ts), "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.ListTodosByUserId(ctx, userId)
 }
--- pkg/user/logging.go
+++ pkg/user/logging.go
@@ -13,35 +13,35 @@ type basicLoggingService struct {
 
 func (s *basicLoggingService) CreateUser(ctx context.Context, name string) (u User, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "create", "name", name, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "create", "name", name, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.CreateUser(ctx, name)
 }
 
 func (s *basicLoggingService) DeleteUser(ctx context.Context, userId string) (u User, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "delete", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "delete", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.DeleteUser(ctx, userId)
 }
 
 func (s *basicLoggingService) UpdateUser(ctx context.Context, user User) (u User, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "update", "updatedName", user.Name, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "update", "updatedName", user.Name, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.UpdateUser(ctx, user)
 }
 
 func (s *basicLoggingService) GetUserById(ctx context.Context, userId string) (u User, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "get", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "get", "name", u.Name, "userId", u.Id, "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.GetUserById(ctx, userId)
 }
 
 func (s *basicLoggingService) ListUsers(ctx context.Context) (us []User, err error) {
 	defer func(begin time.Time) {
-		s.logger.Log("operation", "list", "len", len(us), "took", time.Since(begin), "err", err)
+		_ = s.logger.Log("operation", "list", "len", len(us), "took", time.Since(begin), "err", err)
 	}(time.Now())
 	return s.Service.ListUsers(ctx)
 }
